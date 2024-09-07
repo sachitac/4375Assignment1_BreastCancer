6b5780661074167325fabcec8bd9b5ee5ccb9ddc
b-anjana <anjanarbha@gmail.com>
2024-09-07 14:41:01 -0500

    added gradient descent method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 def process_data():
     # fetch dataset
@@ -20,21 +24,34 @@ def process_data():
     label_encoder = LabelEncoder()
     y_encode = label_encoder.fit_transform(y_clean.values.ravel())
 
-    # display first 5 rows of clean data
-    print("First 5 rows of x_cleaned\n", x_clean.head())
-    print("First 5 rows of y encoded\n", y_encode[:5])
-
     return x_clean, y_encode  # return cleaned data for further processing
 
 def standardize(x_clean):
     scaler = StandardScaler()
     x_scaled = scaler.fit_transform(x_clean)  # scale the features
-    print("First 5 rows of standardized x\n", x_scaled[:5])
     return x_scaled
 
+def split(x_clean, y_encode):
+    x_train, x_test, y_train, y_test = train_test_split(x_clean, y_encode, random_state=104,  test_size=0.25,  shuffle=True) 
+    return x_train, x_test, y_train, y_test
+
+def train(x_train, y_train):
+    sgd_regressor = SGDRegressor(max_iter=1000, tol=1e-3, learning_rate="invscaling")
+    sgd_regressor.fit(x_train, y_train)
+    return sgd_regressor
+
+def evaluate(model, x_test, y_test):
+    y_pred = model.predict(x_test)
+    r2 = r2_score(y_test, y_pred)
+    return r2
+
 def main():
-    x_clean, y_encode = process_data()  # get cleaned data
-    x_scaled = standardize(x_clean)  # standardize features
+    x_clean, y_encode = process_data()
+    x_scaled = standardize(x_clean) 
+    x_train, x_test, y_train, y_test = split(x_scaled, y_encode) 
+    model = train(x_train, y_train)
+    r2 = evaluate(model, x_test, y_test)
+    print(r2)
 
 if __name__ == "__main__":
     main()
